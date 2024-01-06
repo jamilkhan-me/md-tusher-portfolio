bf5bdd006831453d966f86bd18d3c440e78d76a6
jamil Khan <jamilkhan.nu@gmail.com>
2024-01-06 23:06:52 +0000

    bug fixed

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -12,33 +12,39 @@ const Work = () => {
         <div class="pt-24">
           <img class="rounded-xl w-full" src={project1} alt="project1" />
           <div className="flex flex-row pt-4">
-            <h1 class="mr-2 text-4xl">Pleo</h1>
+            <h1 class="mr-2 text-4xl">Britmarksolution</h1>
             <span class="flex items-center text-2xl">
               <FaArrowRightLong />
             </span>
           </div>
-          <h2 class="text-2xl">Onboarding employees in-app</h2>
+          <h2 class="text-2xl pt-2">
+            Digital Marketing and Web Development start-up
+          </h2>
         </div>
         <div class="flex flex-row justify-between py-10 w-full">
           <div class="w-1/2">
             <img class="rounded-xl" src={project2} alt="project2" />
             <div className="flex flex-row pt-4">
-              <h1 class="mr-2 text-4xl">Kernel</h1>
+              <h1 class="mr-2 text-4xl">Cambridge lead</h1>
               <span class="flex items-center text-2xl">
                 <FaArrowRightLong />
               </span>
             </div>
-            <h2 class="text-2xl">Marketing site design and build</h2>
+            <h2 class="text-2xl pt-2">
+              Immigration and international student consultency{" "}
+            </h2>
           </div>
           <div class="w-1/2">
             <img class="rounded-xl" src={project3} alt="project2" />
             <div className="flex flex-row pt-4">
-              <h1 class="mr-2 text-4xl">Freelance Things</h1>
+              <h1 class="mr-2 text-4xl">Olympia care</h1>
               <span class="flex items-center text-2xl">
                 <FaArrowRightLong />
               </span>
             </div>
-            <h2 class="text-2xl">Weekly things for freelancers</h2>
+            <h2 class="text-2xl pt-2">
+              International recruiting firm and social care home
+            </h2>
           </div>
         </div>
       </div>
